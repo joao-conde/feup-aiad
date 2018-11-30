ac36306fd57d8a4a2d412deccd1d46267ff56d6f
João Conde <up201503256@fe.up.pt>
2018-11-30 17:35:46 +0000

    New entry point for application to run multiple simulations

---
+++ src/main/DataGenerator.java
@@ -0,0 +1,48 @@
+package main;
+
+import java.io.File;
+
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.ContainerController;
+import jade.wrapper.StaleProxyException;
+import utilities.MarketLogger;
+
+/*
+ * DataGenerator
+ * 
+ * Responsible for booting up JADE runtime and main container aswell
+ * as one agent that will re-run the auction market simulation multiple
+ * times
+ */
+public class DataGenerator {
+
+	
+	private Runtime jadeRt;
+	private ContainerController mainContainer;
+	
+	public static void main(String[] args) {
+		new DataGenerator(Integer.parseInt(args[0]));
+	}
+	
+	public DataGenerator(Integer simulations) {
+		try {
+			MarketLogger.calculateLogPath();
+			new File(MarketLogger.logPath).mkdirs();
+			this.initializeContainers();
+			this.initializeSimulatorAgent(simulations);
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		}	
+	}
+	
+	public void initializeContainers() {
+		this.jadeRt = Runtime.instance();
+		this.mainContainer = jadeRt.createMainContainer(new ProfileImpl());
+	}
+	
+	public void initializeSimulatorAgent(Integer simulations) {
+		
+	}
+
+}
